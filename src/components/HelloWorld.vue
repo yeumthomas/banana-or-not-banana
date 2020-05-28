b5fa26261891032a8d3e975877ba409da77b27f6
Thomas <yeum.thomas@gmail.com>
2020-05-27 22:02:12 -0500

    first banana commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <img crossorigin="anonymous" id="img" src="https://encrypted-  
+      tbn0.gstatic.com/imagesq=tbn:ANd9GcQ0ucPLLnB4Pu1kMEs2uRZ
+      ISegG5W7Icsb7tq27blyry0gnYhVOfg" alt="Dog" ref="dogId" >
+    <div class="row">
+      <div class="col">
+        <b-list-group>
+          <b-list-group-item v-for="tensor in tensors" 
+              v-bind:key="tensor.className">
+            {{ tensor.className }} - {{ tensor.probability }}
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  private readonly classifier: ImageClassifier = new ImageClassifier();
+  private tensors: TensorInformation[] | null = null;
+  
+  // occurs right as page loads
+  constructor() {
+    super();
+    // this is why we need $nextTick
+    this.Classify();
+  }
+
+  public Classify(): void {
+    // nextTick is when DOM rerenders, so when this occurs, we will have access to image we want to classify
+    this.$nextTick().then(async () => {
+      // so we can get info in the img element without calling getElementById
+      const dog = this.$refs['dogId'];
+      // if dog image exists and this.tensors not already running
+      if (dog !== null && !this.tensors) {
+        // cast dog as HTMLImageElement
+        const image = dog as HTMLImageElement;
+        // run classification script
+        this.tensors = await this.classifier.Classify(image);
+      }
+    })
+  }
 }
 </script>
 
