7ded5765349dd0c0a8d839e38d1a2efa0f45232a
Thomas Yeum <yeum.thomas@gmail.com>
2020-06-09 13:54:34 -0500

    Added .json imports configuration

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,17 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
+
+// This will allow you to load `.json` files from disk
+
+declare module "*.json"
+{ const value: any;
+  export default value;
+}
+
+// This will allow you to load JSON from remote URL responses
+
+declare module "json!*"
+{ const value: any;
+  export default value;
+}
